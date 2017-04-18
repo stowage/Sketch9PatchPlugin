9cde8523d6f4784e5c6f7da7246531dda470b8b9
Sergey Fedoseev <sfedoseev@eastbanctech.com>
2017-04-18 04:10:47 -0400

    Sketch 9-Patch Plugin initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sketch9PatchPlugin
 Sketch 9-Patch experiment
+
+![9-Patch in sketch](https://github.com/stowage/Sketch9PatchPlugin/raw/master/9patch.gif "9-Patch Editor")
---
+++ Sketch9Patch.sketchplugin/Contents/Sketch/Sketch9Patch.framework/Versions/A/Headers/Sketch9PatchPlugin.h
@@ -0,0 +1,16 @@
+//
+//  SketchPluginDemo.h
+//  SketchPluginDemo
+//
+//  Created by Sergey on 1/23/17.
+//  Copyright © 2017 Acme Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Sketch9PatchPlugin : NSObject
+
+
+-(void)processDocument:(id)document;
+
+@end
